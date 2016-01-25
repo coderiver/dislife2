d4b40600b41283b9c667816dd26effc20c609138
whats0n <mrdvv@mail.ru>
2016-01-25 13:32:44 +0200

    update

--- build/js/app.js
+++ build/js/app.js
@@ -29,4 +29,8 @@ $(document).ready(function() {
 		$(this).siblings().removeClass('is-active');
 	});
 
+	$('.js-mini-search-btn').click(function() {
+		$('.js-mini-search-input').toggleClass('is-active');
+	});
+
 });
