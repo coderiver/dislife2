7479ee5c5c897dee5a125bc155cc680a68e054df
yurayarosh <yura12323@gmail.com>
2018-06-18 18:06:50 +0300

    add main adaptive part

--- src/js/new-app-2.js
+++ src/js/new-app-2.js
@@ -1,15 +1,44 @@
 $(document).ready(function() {
   //map
-  ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-      center: [55.7555282, 37.6144813],
-      zoom: 14,
-      scrollZoom: false,
-      controls: []
-    }, {
-      searchControlProvider: 'yandex#search'
-    });
-    myMap.behaviors.disable('scrollZoom');
-    myMap.behaviors.disable('drag');
-  });
+  function setMap() {
+    const map = document.getElementById('map');
+    if (map) {
+      ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+          center: [55.7555282, 37.6144813],
+          zoom: 14,
+          scrollZoom: false,
+          controls: []
+        }, {
+          searchControlProvider: 'yandex#search'
+        });
+        myMap.behaviors.disable('scrollZoom');
+        myMap.behaviors.disable('drag');
+      });
+    };
+
+  };
+  setMap();
+  
+
+  //slider
+  function setMobSlider() {
+    const $slider = $('.js-mob-slider');
+    $slider.slick({
+      nextArrow: $('.js-mob-slider-next'),
+      prevArrow: $('.js-mob-slider-prev'),
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      responsive: [
+      {
+       breakpoint: 660,
+       settings: {
+       }
+     },
+     ]
+   });
+  };
+  setMobSlider();
+
+
 });
